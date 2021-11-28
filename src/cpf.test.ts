077d6a3a7ea78aa21a1ca497d5b56eb6124e7414
Alexandre Guerra <agmguerra@gmail.com>
2021-11-28 11:04:18 -0300

    refactoring do validador de cpf

---
+++ src/cpf.test.ts
@@ -0,0 +1,25 @@
+import isCpfValido from "./cpf";
+
+test("Cpf não deve ser null", function () {
+    expect(isCpfValido(null)).toBe(false);
+});
+
+test("Cpf não deve ser undefined", function() {
+    expect(isCpfValido(undefined)).toBe(false);
+})
+
+test("Cpf não pode ter todos os digitos iguais", function() {
+    expect(isCpfValido("111.111.111-11")).toBe(false);
+})
+
+test("Cpf inválido", function() {
+    expect(isCpfValido("123.456.789-99")).toBe(false);
+})
+
+test("Cpf válido", function() {
+    expect(isCpfValido("935.411.347-80")).toBe(true);
+})
+
+
+
+
